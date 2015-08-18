edd60da17c2655951ce83bf461476624a33fc2d6
Tanner May <tannerjmay@gmail.com>
2015-08-18 09:45:42 -0600

    The teachings of padjen

--- ruby_book/ex11/ex11.rb
+++ ruby_book/ex11/ex11.rb
@@ -6,3 +6,11 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+health = age.to_i + height.to_i + weight.to_i
+puts health
+
+list = [age, height, weight]
+another_list = ["3", 2, true]
+a, b, c = list
+d = list.first
