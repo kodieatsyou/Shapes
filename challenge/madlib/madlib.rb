4ddc26868c7dd7dfc6f6f7c260ebb63dd9584704
Tanner May <tannerjmay@gmail.com>
2015-08-25 09:32:26 -0600

    fixed

---
+++ challenge/madlib/madlib.rb
@@ -0,0 +1,19 @@
+def input
+		gets.chomp
+end
+def get_a_word(word_type)
+		puts "Enter a #{word_type}."
+		input
+end
+words = []
+puts "Fill in the story!"
+
+words.push(get_a_word("noun"))
+words.push(get_a_word("noun"))
+words.push(get_a_word("body part"))
+
+puts """
+There was a #{words.shift} he was on his way to the #{words.shift} 
+On his way there he fell and broke his #{words.shift} 
+Then he died. The End
+"""
