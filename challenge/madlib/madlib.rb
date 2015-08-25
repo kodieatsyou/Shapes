41a793fe6cfa3a9b72e7fb265da72f39b2e709f6
Tanner May <tannerjmay@gmail.com>
2015-08-25 09:55:06 -0600

    Multiple stories and menu

--- challenge/madlib/madlib.rb
+++ challenge/madlib/madlib.rb
@@ -1,19 +1,55 @@
+def menu
+	puts"Choose a story (1 or 2!)"
+	story_num = $stdin.gets.chomp.to_i
+	if story_num == 1
+		story1
+	elsif story_num == 2
+		story2
+	elsif story_num != 1 || 2
+		menu
+	end
+end
+			
 def input
-		gets.chomp
+	gets.chomp
 end
 def get_a_word(word_type)
-		puts "Enter a #{word_type}."
-		input
+	puts "Enter a #{word_type}."
+	input
 end
-words = []
-puts "Fill in the story!"
 
-words.push(get_a_word("noun"))
-words.push(get_a_word("noun"))
-words.push(get_a_word("body part"))
 
-puts """
-There was a #{words.shift} he was on his way to the #{words.shift} 
-On his way there he fell and broke his #{words.shift} 
-Then he died. The End
-"""
+def story1
+	words = []
+	puts "Story 1!"
+
+	words.push(get_a_word("noun"))
+	words.push(get_a_word("noun"))
+	words.push(get_a_word("body part"))
+
+	puts """
+		There was a #{words.shift} he was on his way to the #{words.shift} 
+	On his way there he fell and broke his #{words.shift} 
+	Then he died. The End
+	"""
+end
+
+def story2
+	words = []
+	puts "Story 2!"
+
+	words.push(get_a_word("name"))
+	words.push(get_a_word("noun"))
+	words.push(get_a_word("verb"))
+	words.push(get_a_word("BodyPart"))
+	words.push(get_a_word("Place"))
+
+	puts """
+		There once was man named #{words.shift} he was sitting
+	in his yard thinking about #{words.shift} while he was 
+	in his yard another man came up and #{words.shift}'ed the
+	man in the #{words.shift} then the man ran to the #{words.shift}
+	THE END!
+	"""
+end
+menu
