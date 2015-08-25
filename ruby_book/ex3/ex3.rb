2d3c75f49e5d18af5655256d376d5ca90fe35b12
Tanner May <tannerjmay@gmail.com>
2015-08-25 10:19:52 -0600

    Exercise 3 yo

---
+++ ruby_book/ex3/ex3.rb
@@ -0,0 +1,34 @@
+#Write "I will now count..."
+puts "I will now count my chickens:"
+
+#Count Hens
+puts "Hens #{25 + 30 / 6}"
+#Count Roosters
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Print I will now count the eggs
+puts "Now I will count the eggs:"
+
+#Count eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Ask question
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Solve
+puts 3 + 2 < 5 - 7
+
+#Ask questions
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+#Say stuff
+puts "Oh, that's why it's false."
+
+#Say more stuff
+puts "How about some more."
+
+#More questions
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}" 
